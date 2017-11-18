a0cd59325e681699f589e8601e5378b5c159a459
Faith <fmugure@intracitygeeks.org>
2017-11-18 03:53:31 +0000

    argc argv learn

--- argsII.c
+++ argsII.c
@@ -1,8 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) //YOU CAN USE STRING ARGV[] OR CHAR* ARGV[]
-//int main(int argc, char* argv[]) //ARGC=ARGUMENT COUNT I.E. # OF INPUTS BY USER
+//int main(int argc, string argv[])
+int main(int argc, char* argv[]) //ARGC=ARGUMENT COUNT I.E. # OF INPUTS BY USER
 {
     int i;
     // PRINT CONTENTS OF ARGC C, %D=SUPPLYING THE STRING WITH AN INTEGER VALUE, THE INTEGER TO SUPPLY IS THE VARIABLE IN ARGC
@@ -16,8 +16,3 @@ int main(int argc, string argv[]) //YOU CAN USE STRING ARGV[] OR CHAR* ARGV[]
 
     return 0;
 }
-//FIRST ITERATION. SEE ARGSII AFTER THIS
-//THIS PRINTS THE FOLLOWING:
-//argc = 1
-//What's in argv[]
-//argv[0] = ./args
